25ae0d810b3a3024b0edfa1fdedd917a5da15fdb
AnastasiyaYudina <106549267+AnastasiyaYudina@users.noreply.github.com>
2022-06-28 22:34:58 +0300

    Delete Task0 directory

--- Task0/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите целое число: ");
-int num = Convert.ToInt32 (Console.ReadLine());
-int squere=num*num;
-Console.WriteLine($"Квадрат числа {num} = {squere}");
